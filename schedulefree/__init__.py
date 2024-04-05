5e225ef73178c2a25be85ab4cbd48c0ccdbb54f5
Aaron Defazio <adefazio@fb.com>
2024-04-05 14:56:12 +0000

    Updated

--- schedulefree/__init__.py
+++ schedulefree/__init__.py
@@ -3,7 +3,7 @@
 # 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-from adamw_schedulefree_closure import AdamWScheduleFreeClosure
-from adamw_schedulefree import AdamWScheduleFree
-from sgd_schedulefree_closure import SGDScheduleFreeClosure
-from sgd_schedulefree import SGDScheduleFree
+from .adamw_schedulefree_closure import AdamWScheduleFreeClosure
+from .adamw_schedulefree import AdamWScheduleFree
+from .sgd_schedulefree_closure import SGDScheduleFreeClosure
+from .sgd_schedulefree import SGDScheduleFree
